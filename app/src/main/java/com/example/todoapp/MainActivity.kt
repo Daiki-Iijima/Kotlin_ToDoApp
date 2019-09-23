7d8a6cc4529e18344d66cb51e6ffcc3a14360dfb
daiki_iijima <daiki.applecreate@gmail.com>
2019-09-23 21:15:56 +0900

    画面遷移の方法を1つのkt内で完結できる方法に変更

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,6 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 import android.content.Intent
@@ -13,41 +9,51 @@ import androidx.core.app.ComponentActivity
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    var items = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setMainActivity()
+    }
+
+    private fun setMainActivity()
+    {
         setContentView(R.layout.activity_main)
 
+        val adapter  = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items)
 
+        findViewById<ListView>(R.id.TodoTableView).adapter = adapter
 
-        val items = ArrayList<String>()
-        items.add("test")
-
-
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items)
-        TodoTableView.adapter = adapter
-
-
-        TodoTableView.setOnItemClickListener{ _,view,_,_->
+        findViewById<ListView>(R.id.TodoTableView).setOnItemClickListener{ _,view,_,_->
             val textView = view.findViewById<TextView>(android.R.id.text1)
             Toast.makeText(this,"クリックされたぞ : ${textView.text}",Toast.LENGTH_SHORT).show()
         }
 
-        val addButton = this.findViewById<Button>(R.id.AddButton)
-
-        addButton.setOnClickListener()
+        this.findViewById<Button>(R.id.AddButton).setOnClickListener()
         {
-            items.add("test")
-            TodoTableView.adapter = adapter
-
-            //  情報追加画面に遷移
-            val intent = Intent(application, SubActivity::class.java)
-            startActivity(intent)
+            setAddDataActivity()
         }
+
+    }
+
+    private  fun setAddDataActivity()
+    {
+        setContentView(R.layout.subavtivity_main)
+
+        this.findViewById<Button>(R.id.AddButton).setOnClickListener()
+        {
+            items.add(
+                this.findViewById<TextView>(R.id.AddTextView).text.toString()
+            )
+
+            setMainActivity()
+        }
+
     }
 }
